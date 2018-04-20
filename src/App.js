c79392505e0ac86092b6e4578e098ef1fb78b79e
Ozzy Cardenas <ocardenas789@me.com>
2018-04-19 22:24:05 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Wrapper from "./components/Wrapper"
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Wrapper>
+        
+        </Wrapper>
+      </div>
+    );
+  }
+}
+
+export default App;
